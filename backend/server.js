a85434f5aadeb8883181c9dec185da0f4bdc7cfd
Ion Gadarenco <ion.gadarenco@gmail.com>
2023-06-19 22:47:47 +0300

    comitul meu

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const moment = require('moment');
+const fs = require('fs');
+
 
 const app = express();
-const port = 6000;
+const port = 6010;
 
 app.use(express.static('public'));
 
@@ -10,7 +12,19 @@ app.get('/time', (req, res) => {
   const currentTime = moment().format('HH:mm:ss');
   const isDaytime = moment().isBetween('06:00:00', '18:00:00');
 
+  const data = { isDaytime };
+  const jsonData = JSON.stringify(data);
+
+  fs.writeFile('db.json', jsonData, (err) => {
+    if (err) {
+      console.error('Error saving data:', err);
+    } else {
+      console.log('Data saved successfully.');
+    }
+  });
+
   res.json({ currentTime, isDaytime });
+
 });
 
 app.listen(port, () => {
