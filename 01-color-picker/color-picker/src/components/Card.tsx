f7be85dbf322c84bcd1dc43565fe206630bba2b9
DumitruGirdei <girdeidumitru@gmail.com>
2023-06-21 11:29:14 +0300

    Background

--- 01-color-picker/color-picker/src/components/Card.tsx
+++ 01-color-picker/color-picker/src/components/Card.tsx
@@ -14,8 +14,43 @@ const Card: FC = () => {
       setIsChecked(!isChecked);
     };
 
-  const handleColorChange = (newColor: string) => {
-      setColor(newColor)
+
+    const calculateHSL = (red_color: number, green_color: number, blue_color: number) => {
+        red_color /= 255;
+        green_color /= 255;
+        blue_color /= 255;
+
+        const max = Math.max(red_color, green_color, blue_color);
+        const min = Math.min(red_color, green_color, blue_color);
+
+        let nuanta = 0;
+        let saturation = 0;
+        let lightness = (max + min) / 2;
+
+        if (max !== min) {
+            const delta = max - min;
+            saturation = lightness > 0.5 ? delta / (2 - max - min) : delta / (max + min);
+
+            if (max === red_color) {
+                nuanta = (green_color - blue_color) / delta + (green_color < blue_color ? 6 : 0);
+            } else if (max === green_color) {
+                nuanta = (blue_color - red_color) / delta + 2;
+            } else {
+                nuanta = (red_color - green_color) / delta + 4;
+            }
+            nuanta /= 6;
+
+
+            nuanta = Math.round(nuanta * 360);
+            saturation = Math.round(saturation * 100);
+            lightness = Math.round(lightness * 100);
+
+            return `hsl(${nuanta}, ${saturation}%, ${lightness}%)`;
+        }
+
+    }
+        const handleColorChange = (newColor: string) => {
+         setColor(newColor)
     }
 
   // backend request
@@ -29,20 +64,27 @@ const Card: FC = () => {
    const green: number = parseInt(color.substring(3, 5), 16);
    const blue: number = parseInt(color.substring(5, 7), 16);
 
-  const rgb: number[] = [red, green, blue];
- 
-   const backgroundStyle = {
-      backgroundImage: `radial-gradient(at top left, hsl(${rgb[0]}, 50%, 50%), transparent),
-                      radial-gradient(at top right, hsl(${rgb[1]}, 50%, 50%), transparent),
-                      radial-gradient(at bottom left, hsl(${rgb[2]}, 50%, 50%), transparent),
-                      radial-gradient(at bottom right, hsl(${rgb[0]}, 50%, 50%), transparent)`
+  //const rgb: number[] = [red, green, blue];
 
-    };
+        const backgroundStyle = {
+            backgroundImage: `
+      radial-gradient(at top left, ${calculateHSL(red, green, blue)}, transparent),
+      radial-gradient(at top right, ${calculateHSL(green, red, blue)}, transparent),
+      radial-gradient(at bottom left, ${calculateHSL(blue, red, green)}, transparent),
+      radial-gradient(at bottom right, ${calculateHSL(red, green, blue)}, transparent)
+    `
+        };
+
 
     const cardStyle = {
         background: isChecked ? '#141414' : 'white',
         color: isChecked ? 'white' : "black"
 };
+
+
+
+
+
 
     return (
         <div className="container" style={backgroundStyle}>
@@ -74,13 +116,13 @@ const Card: FC = () => {
 
             <ColorPicker onColorChange={handleColorChange}/>
             <div className='text__inside'>
-              <span>background: </span>
-              <ul>
-                <li>radial-gradient(at top left, hsl({rgb[0]}, 50%, 35%), transparent)</li>
-                <li>radial-gradient(at top right, hsl({rgb[1]}, 50%, 35%), transparent)</li>
-                <li>radial-gradient(at bottom left, hsl({rgb[2]}, 50%, 35%), transparent)</li>
-                <li>radial-gradient(at bottom right, hsl({rgb[0]}, 50%, 35%), transparent)</li>
-              </ul>
+                <span>background: </span>
+                <ul>
+                    <li>radial-gradient(at top left, {calculateHSL(red, green, blue)}, transparent)</li>
+                    <li>radial-gradient(at top right, {calculateHSL(green, red, blue)}, transparent)</li>
+                    <li>radial-gradient(at bottom left, {calculateHSL(blue, red, green)}, transparent)</li>
+                    <li>radial-gradient(at bottom right, {calculateHSL(red, green, blue)}, transparent)</li>
+                </ul>
 
             </div>
           </div>
